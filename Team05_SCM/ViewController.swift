b718af99b96952669790ba6320940fec0b143cb4
Ritik Ranjan <ritik0ranjan+github@gmail.com>
2024-12-02 12:11:19 +0530

    Initial Commit

---
+++ Team05_SCM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team05_SCM
+//
+//  Created by Ritik Ranjan on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
