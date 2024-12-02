f3717f5b81fff9dd2d4d86e251918d9a0b73351c
Nupur Sharma <write2nupu@gmail.com>
2024-12-02 14:54:10 +0530

    Add new file

---
+++ Team05_SCM/SampleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SampleViewController.swift
+//  Team05_SCM
+//
+//  Created by Batch-2 on 02/12/24.
+//
+
+import UIKit
+
+class SampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
